8f62137e7ce8c782b95c031599cd042cfa36ec73
Lukasz Malyszko <lukas.malyszko@gmail.com>
2023-03-25 14:56:04 +0100

    0.03 ver

--- src/pages/home-page/scripts/home-page.js
+++ src/pages/home-page/scripts/home-page.js
@@ -3,18 +3,35 @@
 
 import '../styles/home-page.scss'
 
-import { setupCounter } from '../../../global-scripts/scripts/counter/counter.js'
+//  import { setupCounter } from '../../../global-scripts/scripts/counter/counter.js'
 
-setupCounter(document.querySelector('#counter'))
+//  setupCounter(document.querySelector('#counter'))
 
+const buttonBasic = document.getElementById("button-basic");
+const buttonPro = document.getElementById("button-pro");
+const buttonEnter = document.getElementById("button-enter");
+const loaders = document.getElementsByClassName("load");
 
-let button = document.getElementsByTagName("button");
-let loader = document.getElementById("loader");
-function loading() {
-    loader.addClass("loader");
-
+function initLoading() {
+    
+    
+        buttonBasic.classList.add("hidden");
+        buttonPro.classList.add("hidden");
+        buttonEnter.classList.add("hidden");
+      
+    for(let i = 0; i < loaders.length; i++){
+        let loader = loaders[i];
+        loader.classList.add("loader");
+        setTimeout(()=> {
+            loader.style.display = "none"
+            buttonBasic.classList.remove("hidden");
+            buttonPro.classList.remove("hidden");
+            buttonEnter.classList.remove("hidden");
+        }, 3000);
+    }
 }
 
-
-    button.addEventListener("click", loading);
-
+    buttonBasic.addEventListener("click", initLoading);
+    buttonPro.addEventListener("click", initLoading);
+    buttonEnter.addEventListener("click", initLoading);
+  
